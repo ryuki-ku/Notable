f01a5f217edd4fcce7449223c63e007188980b42
phuong <thephuong3011@gmail.com>
2023-07-27 20:54:11 +0700

    create two new file inside app folder

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Notable',
+    description: 'Create and discover new way of AI searching tools'
+}
+
+const RootLayout = () => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/> {/*changing the background's homepage*/}
+            </div>
+
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout;
