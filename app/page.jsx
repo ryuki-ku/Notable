c15d4b80c213114b681e6a4850fc93d201bda7be
phuong <thephuong3011@gmail.com>
2023-07-28 10:21:16 +0700

    adding new header

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
     <section className="w-full flex-center flex-col">
-        
+        <h1 className="head_text text-center">
+          Discover $ share
+        </h1>
     </section>
   )
 }
