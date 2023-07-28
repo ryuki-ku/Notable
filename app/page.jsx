d8730940716136c3e1313438eaa0b0212c303c05
phuong <thephuong3011@gmail.com>
2023-07-28 19:07:43 +0700

    ...

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,8 @@ const HomePage = () => {
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-          Discover $ share
+          Discover & share
+          <br />
         </h1>
     </section>
   )
