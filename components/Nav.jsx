5f84cc48f38c6ad4b9800b5609f9bdb5535178ba
phuong <thephuong3011@gmail.com>
2023-07-31 10:18:11 +0700

    Make sign in option for user(not complete yet)

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,16 @@ import {signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
  const isUserLoggedIn = true;
+ const [providers, setProviders] = useState(null);
 
+ useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+    setProviders();
+ }, []);
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 justify-center items-center'>
@@ -45,10 +54,25 @@ const Nav = () => {
             </div>
           ) : (
             <>
-            
+              { providers &&
+                Object.values(providers).map((provider) => 
+                (
+                  <button
+                    type='button'
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                    className='black_btn'
+                  >
+                    Sign In
+                  </button>
+                ))}
             </>
           )}
       </div>
+      {/*Mobile responsive */}
+
+
+
     </nav>
   )
 }
