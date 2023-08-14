8a8119804fb0978414938c096571889186988410
phuong <thephuong3011@gmail.com>
2023-08-14 09:43:16 +0700

    finish create and copy note card information

---
+++ app/profile/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React , {useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
