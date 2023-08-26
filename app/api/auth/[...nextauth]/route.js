3fb6d52a6e9da05a6f6a133acd88465dae6746d1
phuong <thephuong3011@gmail.com>
2023-08-26 20:46:24 +0700

    change from image to picture

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -36,7 +36,7 @@ const handler = NextAuth({
                     await User.create({
                         email: profile?.email,
                         username: profile?.name.replace(/\s+/g, "").toLowerCase(),
-                        image: user.image
+                        image: user.picture
                     })
                 }
                 return true;
